8134b94dd897f2f0d4cc006b4395d2ed5a0bc8e0
Q <foldervapp@gmail.com>
2023-03-17 22:05:45 +0800

    android

---
+++ examples/android/app/src/main/java/com/folderv/whisper/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.folderv.whisper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
